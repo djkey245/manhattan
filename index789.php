a4d5f99c73aa993e1077577b698a0c90c9b2fd25
Djkey <djkey245@gmail.com>
2017-07-03 16:29:54 +0300

    initial commit

---
+++ index789.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title></title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<table border="1" class="main">
+
+    <tr>
+ 	<td class="top" colspan="2">top</td>
+    </tr>
+    <tr>
+ 	<td class="search">search</td>
+ 	<td class="topmenu">topmenu</td>
+    </tr>
+    <tr>
+ 	<td class="news">news</td>
+ 	<td class="text">text</td>
+    </tr>
+    <tr>
+ 	<td class="copyright">copyright</td>
+ 	<td class="bottommenu">bottommenu</td>
+    </tr>
+
+</table>
+</body>
+</html>
