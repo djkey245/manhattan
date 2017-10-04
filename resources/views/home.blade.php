2db16d547e4b15f2ef7b0c9fd7a6cc85f5b2b6e9
Djkey <djkey245@gmail.com>
2017-10-04 15:50:08 +0300

    Refactoring Black-theme 0.15

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,78 @@
             <div class="container-fluid" >
 
                 <div class="row-fluid" >
-
+                    <div class="hidden-alert" id="hidden-alert">
+                        <div class="row-fluid">
+                            <div class="btn-x">
+                                <button class="btn-xs"  onclick="link_alert_none()">X</button>
+                            </div>
+                            <div id="actual1">
+                                <?php $i = 0;?>
+                                @foreach($notactuals['1']['0'] as $peopler)
+                                    @foreach($peoples as $people)
+                                        @if($people->id == $peopler)
+                                            <a href="/list/{{$people->id}}">{{$people->surname}} {{$people->name}}</a><br>
+                                        @endif
+                                    @endforeach
+                                    @if(++$i >= 20)
+                                        @break
+                                        @endif
+                                @endforeach
+                            </div>
+                            <div id="actual2">
+                                <?php $i = 0;?>
+                            @foreach($notactuals['2']['0'] as $peopler)
+                                    @foreach($peoples as $people)
+                                        @if($people->id == $peopler)
+                                            <a href="/list/{{$people->id}}">{{$people->surname}} {{$people->name}}</a><br>
+                                        @endif
+                                    @endforeach
+                                        @if(++$i >= 20)
+                                            @break
+                                        @endif
+                                @endforeach
+                            </div>
+                            <div id="actual3">
+                                <?php $i = 0;?>
+                                @foreach($notactuals['3']['0'] as $peopler=>$key)
+                                    @foreach($peoples as $people)
+                                        @if($people->id == $peopler)
+                                            <a href="/list/{{$people->id}}">{{$people->surname}} {{$people->name}}</a><br>
+                                        @endif
+                                    @endforeach
+                                        @if(++$i >= 20)
+                                            @break
+                                        @endif
+                                @endforeach
+                            </div>
+                            <div id="actual4">
+                                <?php $i = 0;?>
+                                @foreach($notactuals['4']['0'] as $peopler)
+                                    @foreach($peoples as $people)
+                                        @if($people->id == $peopler)
+                                            <a href="/list/{{$people->id}}">{{$people->surname}} {{$people->name}}</a><br>
+                                        @endif
+                                    @endforeach
+                                        @if(++$i >= 20)
+                                            @break
+                                        @endif
+                                @endforeach
+                            </div>
+                            <div id="actual5">
+                                <?php $i = 0;?>
+                                @foreach($notactuals['5']['0'] as $peopler)
+                                    @foreach($peoples as $people)
+                                        @if($people->id == $peopler)
+                                            <a href="/list/{{$people->id}}">{{$people->surname}} {{$people->name}}</a><br>
+                                        @endif
+                                    @endforeach
+                                        @if(++$i >= 20)
+                                            @break
+                                        @endif
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                         <div class="col-md-6 ">
                             <div class="block">
 
@@ -21,20 +92,16 @@
 
                         <div class="col-md-6 " id="other" >
                             <div class="block">
-                                    <div class="hidden-alert">
-                                        <div class="row-fluid">
-                                            re
-                                        </div>
-                                </div>
+
                         @foreach($importants as $important)
 
                                 <?php $i = 0; ?>
                                     @if(count($notactuals[$important->id]['0']) > 0)
                                         <div>
                                         @if(count($notactuals[$important->id]['0']) == 1)
-                                            {{$important->info}} y <div class="link-alert" onclick="link_alert()">{{count($notactuals[$important->id]['0'])}}</div>  працівникa!
+                                            {{$important->info}} y <div class="link-alert" id="{{$important->id}}" onclick="link_alert()">{{count($notactuals[$important->id]['0'])}}</div>  працівникa!
                                         @elseif(count($notactuals[$important->id]['0']) > 1)
-                                                {{$important->info}} y <div class="link-alert" onclick="link_alert()">{{count($notactuals[$important->id]['0'])}}</div> працівників!
+                                                {{$important->info}} y <div class="link-alert" id="{{$important->id}}" onclick="link_alert()">{{count($notactuals[$important->id]['0'])}}</div> працівників!
                                         @endif
 
                                             <br>
@@ -43,21 +110,63 @@
                             @endforeach
                                         {{-- script Alert, hover class:link-alert --}}
                                 <script>
-
-
-                                    $(".link-alert").hover(
-                                    function() {
-                                        $(".hidden-alert" ).show();
-                                    },
-
-                                    function() {
-                                        $(".hidden-alert" ).hide();
-                                    }
-                                    );
+                                    var i = 0;
+                                    var X;
+                                    var Y;
+                                    $(document).mousemove(function(e){
+                                        X = e.pageX; // положения по оси X
+                                        Y = e.pageY; // положения по оси Y
+                                    });
+                                    //id = 1
+                                    $("#1").hover(function() {i=0;var elem = document.getElementById("hidden-alert");
+                                    elem.style.cssText = "top: "+Y+"px;left:"+X+"px;";
+                                    $(".hidden-alert").show();
+                                    $("#actual1").show();},
+                                    function() {if(i == 0){$(".hidden-alert" ).hide();
+                                        $("#actual1").hide();
+                                    }});
+                                    // id = 2
+                                    $("#2").hover(function() {i=0;var elem = document.getElementById("hidden-alert");
+                                            elem.style.cssText = "top: "+Y+"px;left:"+X+"px;";
+                                            $(".hidden-alert").show();
+                                            $("#actual2").show();},
+                                        function() {if(i == 0){$(".hidden-alert" ).hide();
+                                            $("#actual2").hide();
+                                        }});
+                                    // id = 3
+                                    $("#3").hover(function() {i=0;var elem = document.getElementById("hidden-alert");
+                                            elem.style.cssText = "top: "+Y+"px;left:"+X+"px;";
+                                            $(".hidden-alert").show();
+                                            $("#actual3").show();},
+                                        function() {if(i == 0){$(".hidden-alert" ).hide();
+                                            $("#actual3").hide();
+                                        }});
+                                    // id = 4
+                                    $("#4").hover(function() {i=0;var elem = document.getElementById("hidden-alert");
+                                            elem.style.cssText = "top: "+Y+"px;left:"+X+"px;";
+                                            $(".hidden-alert").show();
+                                            $("#actual4").show();},
+                                        function() {if(i == 0){$(".hidden-alert" ).hide();
+                                            $("#actual4").hide();
+                                        }});
+                                    // id = 5
+                                    $("#5").hover(function() {i=0;var elem = document.getElementById("hidden-alert");
+                                            elem.style.cssText = "top: "+Y+"px;left:"+X+"px;";
+                                            $(".hidden-alert").show();
+                                            $("#actual5").show();},
+                                        function() {if(i == 0){$(".hidden-alert" ).hide();
+                                            $("#actual5").hide();
+                                        }});
 
                                     function link_alert() {
+                                        i = 1;
+
                                         $(".hidden-alert" ).show();
 
+                                    }
+                                    function link_alert_none() {
+                                        i=0;
+                                        $(".hidden-alert" ).hide();
                                     }
 
 
