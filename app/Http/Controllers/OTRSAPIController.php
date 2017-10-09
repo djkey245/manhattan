45f2824ce040e1416327aeb627e8228e51eb67a7
Djkey <djkey245@gmail.com>
2017-10-09 16:05:30 +0300

    Refactoring Black-theme

--- app/Http/Controllers/OTRSAPIController.php
+++ app/Http/Controllers/OTRSAPIController.php
@@ -9,7 +9,24 @@ use App\Http\Requests;
 class OTRSAPIController extends Controller
 {
     public function index(){
+
         require('Class/OTRSAPI.php');
+        $otrs = new OTRSAPI('http://otrs.softgroup.ua/otrs/rpc.pl','djkey245','Pk5awIDtkf63zfPL');
+        $client = new \SoapClient(null, array(
+            'location'  => 'http://otrs.softgroup.ua/otrs/index.pl',
+            'uri'       => "Core",
+            'trace'     => 1,
+            'login'     => 'djkey245',
+            'password'  => 'Pk5awIDtkf63zfPL',
+            'style'     => SOAP_RPC,
+            'use'       => SOAP_ENCODED));
+        $username = 'djkey245';
+        $password = 'Pk5awIDtkf63zfPL';
+        $TicketID = '809';
+         $otrs->getTicket($TicketID);
+
+dd($otrs->getTicket($TicketID));
 
     }
+
 }
